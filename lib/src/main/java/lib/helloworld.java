22d238d64fd8e857e0b88a9e10e1db82b0eedcbe
yuchen <yuchen@localhost>
2022-04-18 20:40:25 +0800

    mac

---
+++ lib/src/main/java/lib/helloworld.java
@@ -0,0 +1,18 @@
+package lib;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class helloworld {
+private static Logger LOGGER = LoggerFactory.getLogger(helloworld.class);
+
+
+
+public static void main(String[] args) {
+LOGGER.info("Hello World");
+//System.out.println("Hello World");
+}
+}
